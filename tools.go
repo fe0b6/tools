ee2ac388e6cdc6ed60339bb11855a298c1a8a27a
fe0b6 <spirit-dnk@yandex.ru>
2018-04-27 19:37:25 +0300

    add GetMonthName

--- tools.go
+++ tools.go
@@ -284,3 +284,35 @@ func ToJSON(i interface{}) (b []byte) {
 
 	return
 }
+
+// GetMonthName - Возвращаем название месяца
+func GetMonthName(m int) string {
+	switch m {
+	case 1:
+		return "Январь"
+	case 2:
+		return "Февраль"
+	case 3:
+		return "Март"
+	case 4:
+		return "Апрель"
+	case 5:
+		return "Май"
+	case 6:
+		return "Июнь"
+	case 7:
+		return "Июль"
+	case 8:
+		return "Август"
+	case 9:
+		return "Сентябрь"
+	case 10:
+		return "Октябрь"
+	case 11:
+		return "Ноябрь"
+	case 12:
+		return "Декабрь"
+	}
+
+	return ""
+}
