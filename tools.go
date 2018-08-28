27e7c57cda5af2bd9b5d0418594f0c6215663809
fe0b6 <spirit-dnk@yandex.ru>
2018-08-28 13:21:39 +0300

    add IsNil

--- tools.go
+++ tools.go
@@ -429,3 +429,58 @@ func WaitTo(t map[string]int) {
 
 	time.Sleep(nt.Sub(time.Now()))
 }
+
+// IsNil - Проверяем пустая структура или нет
+func IsNil(v interface{}) bool {
+	return isNil(reflect.ValueOf(v))
+}
+func isNil(v reflect.Value) bool {
+	if !v.IsValid() {
+		return true
+	}
+
+	switch v.Kind() {
+	case reflect.Bool:
+		return v.Bool() == false
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32,
+		reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+
+	case reflect.Complex64, reflect.Complex128:
+		return v.Complex() == 0
+
+	case reflect.Ptr, reflect.Interface:
+		return isNil(v.Elem())
+
+	case reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			if !isNil(v.Index(i)) {
+				return false
+			}
+		}
+		return true
+	case reflect.String:
+		return v.Len() == 0
+
+	case reflect.Slice, reflect.Map:
+		return v.IsNil()
+
+	case reflect.Struct:
+		for i, n := 0, v.NumField(); i < n; i++ {
+			if !isNil(v.Field(i)) {
+				return false
+			}
+		}
+		return true
+	// reflect.Chan, reflect.UnsafePointer, reflect.Func
+	default:
+		return v.IsNil()
+	}
+}
