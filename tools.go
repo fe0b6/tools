9cc48d21d1fd195903a0a3edd33d2f98641df6bc
fe0b6 <spirit-dnk@yandex.ru>
2018-08-30 11:05:23 +0300

    add ChunkSliceMapStrInt

--- tools.go
+++ tools.go
@@ -87,6 +87,36 @@ func ChunkSliceInt(arr []int, size int) (ans [][]int) {
 	return
 }
 
+// ChunkSliceMapStrInt - Разбиваем массив map[string]int на несколько
+func ChunkSliceMapStrInt(arr []map[string]int, size int) (ans [][]map[string]int) {
+	msize := len(arr) / size
+	if len(arr)%size != 0 {
+		msize++
+	}
+	ans = make([][]map[string]int, msize)
+
+	l := len(arr)
+	now := 0
+	i := 0
+	for {
+		next := now + size
+
+		if now+size > l {
+			next = l
+		}
+
+		ans[i] = arr[now:next]
+		i++
+
+		if next == l {
+			break
+		}
+		now = next
+	}
+
+	return
+}
+
 // InArray - проверяем содержит ли массив строку
 func InArray(arr []string, str string) (ok bool) {
 	for i := range arr {
